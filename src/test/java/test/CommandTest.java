1303cd0a6918031d11509e1a386c4bed7bf493a0
goldkinn-liu.zijie <lzj@gaojin.com.cn>
2022-03-20 22:16:55 +0800

    add: org
    delt: x

--- src/test/java/test/CommandTest.java
+++ src/test/java/test/CommandTest.java
@@ -3,11 +3,15 @@ package test;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 import org.jarexplorer.ClassInformation;
 import org.jarexplorer.JarExplorer;
 import org.junit.Test;
+
+import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.util.IdUtil;
 
 /**
  * @author LZJ
@@ -38,5 +42,44 @@ public class CommandTest {
 
         // ZipJar.deleteDir(new File(destinationDir));
     }
+
+    /**
+     * mvn test -Dtest=test.CommandTest#genSerNumber
+     * 生成打印条码序列
+     * 规则：3位置工厂编码 + (99999999999999999-日期) + 4位自增序列号
+     */
+    @Test
+    public void genSerNumber() {
+        ConcurrentHashMap<Long,Boolean> concurrentHashMap = new ConcurrentHashMap();
+        new Thread() {
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    functiont();
+                }
+            }
+        }.start();
+
+        new Thread() {
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    functiont();
+                }
+            }
+        }.start();
+        try {
+            Thread.sleep(10000L);
+        }
+        catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void functiont() {
+        Snowflake snowflake = IdUtil.getSnowflake(1, 1);
+        long id = snowflake.nextId();
+        System.out.println(id);
+    }
+
 
 }
